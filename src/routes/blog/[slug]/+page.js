292cb56c8227b7879a066e999df84b8cf3e28b60
Ali Mir <ali.ahmed.mir@gmail.com>
2023-10-09 21:39:25 -0400

    format

--- src/routes/blog/[slug]/+page.js
+++ src/routes/blog/[slug]/+page.js
@@ -1,13 +1,13 @@
 // Import markdown conversion library
-import { marked } from 'marked'
-import {fm} from 'front-matter'
-import { error } from '@sveltejs/kit';
+import {marked} from 'marked';
+import {fm} from 'front-matter';
+import {error} from '@sveltejs/kit';
 
-export function load({ params }) {
-	return {
-		post: {
-			title: `Title for ${params.slug} goes here`,
-			content: `Content for ${params.slug} goes here`
-		}
-	};
+export function load({params}) {
+  return {
+    post: {
+      title: `Title for ${params.slug} goes here`,
+      content: `Content for ${params.slug} goes here`
+    }
+  };
 }
