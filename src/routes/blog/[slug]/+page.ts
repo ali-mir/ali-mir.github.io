b1f5c2f2d5d1b1b0eef858e50aea68d141198847
Ali Mir <ali.ahmed.mir@gmail.com>
2023-10-10 20:41:37 -0400

    convert ts

--- src/routes/blog/[slug]/+page.ts
+++ src/routes/blog/[slug]/+page.ts
@@ -1,13 +1,17 @@
 // Import markdown conversion library
 import {marked} from 'marked';
-import {fm} from 'front-matter';
+// import {fm} from 'front-matter';
+const fs = require('fs')
 import {error} from '@sveltejs/kit';
 
-function parseBlogpost(name) {
+function parseBlogpost(name: string) {
   console.log(`parsing blog post ${name}`);
+
+  console.log(fs);
   return ["metadata", "content"];
 }
 
+/** @type {import('./$types').PageLoad} */
 export function load({params}) {
   const [metadata, content] = parseBlogpost(params.slug);
   return {
