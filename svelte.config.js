cc1a7fc90c1ba8dd17aa9cbd0afe5abc2d9f6b8c
Ali Mir <ali.ahmed.mir@gmail.com>
2023-10-20 11:10:28 -0400

    attempt to fix

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import { vitePreprocess } from '@sveltejs/kit/vite';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
+  paths: { assets: "", base: "/" },
   kit: {
     adapter: adapter(),
   },
