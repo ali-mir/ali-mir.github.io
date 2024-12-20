d992232a488778a11440d9bf0a1d24499f07c108
Ali Mir <ali.ahmed.mir@gmail.com>
2024-12-20 10:07:43 +0600

    formatting

--- src/routes/writing/+page.svelte
+++ src/routes/writing/+page.svelte
@@ -6,64 +6,58 @@
   <title>ali mir / writing</title>
 </svelte:head>
 
-<div class="flex justify-center text-center mx-auto w-96">
+<div class="flex justify-center text-center mx-auto">
   <div class="p-5">
     <h1 class="text-[#C44B4F] font-bold text-3xl">writing</h1>
     <ul class="mt-4">
       <li>
-        <!-- internet slice november-present -->
-        <div class="flex row items-center space-x-4">
+        <div class="grid grid-cols-[1fr_auto] gap-6">
+          <!-- internet slice november-present -->
           <a
             href="/writing/internetslice3_nov_present"
             class="text-[#536c73] font-bold text-lg"
             >internet slice, nov-present</a
           >
-          <p class="text-[#536c73]">Dec 20th, 2024</p>
-        </div>
-        <!-- internet slice august-october -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">Dec 20th, 2024</p>
+
+          <!-- internet slice august-october -->
           <a
             href="/writing/internetslice2_aug_oct"
             class="text-[#536c73] font-bold text-lg">internet slice, aug-oct</a
           >
-          <p class="text-[#536c73]">October 31st, 2024</p>
-        </div>
-        <!-- jury duty -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">October 31st, 2024</p>
+
+          <!-- jury duty -->
           <a href="/writing/juryduty" class="text-[#536c73] font-bold text-lg"
             >jury duty</a
           >
-          <p class="text-[#536c73]">September 14th, 2024</p>
-        </div>
-        <!-- internet slice may-july -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">September 14th, 2024</p>
+
+          <!-- internet slice may-july -->
           <a
             href="/writing/internetslice1_may_july"
             class="text-[#536c73] font-bold text-lg">internet slice, may-july</a
           >
-          <p class="text-[#536c73]">July 31st, 2024</p>
-        </div>
-        <!-- carnegie -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">July 31st, 2024</p>
+
+          <!-- carnegie -->
           <a
             href="/writing/carnegiehall"
             class="text-[#536c73] font-bold text-lg">carnegie hall</a
           >
-          <p class="text-[#536c73]">April 30th, 2024</p>
-        </div>
-        <!-- reading -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">April 30th, 2024</p>
+
+          <!-- reading -->
           <a href="/writing/reading" class="text-[#536c73] font-bold text-lg"
             >back to reading</a
           >
-          <p class="text-[#536c73]">Jan 7th, 2024</p>
-        </div>
-        <!-- hello world -->
-        <div class="flex row items-center space-x-4">
+          <p class="text-[#536c73] text-right">Jan 7th, 2024</p>
+
+          <!-- hello world -->
           <a href="/writing/hello" class="text-[#536c73] font-bold text-lg"
             >hello world</a
           >
-          <p class="text-[#536c73]">Oct 17th, 2023</p>
+          <p class="text-[#536c73] text-right">Oct 17th, 2023</p>
         </div>
       </li>
     </ul>
